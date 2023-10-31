012aa2f0059ecc31a1ea2f0346569b68af30f29e
Andres Chanchi <andfelch@gmail.com>
2023-10-30 20:21:14 -0500

    Adding styles to SubNavbar

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -6,12 +6,16 @@ import './MainLayout.css';
 const MainLayout = ({ children }) => {
   return (
     <div className="main-layout">
-      <Navbar />
-      <SubNavbar />
+      <header>
+        <div className="container">
+        <Navbar />
+        <SubNavbar />
+      	</div>
+      </header>
       <main>{children}</main>
       {/* <Footer /> Cuando esté listo, puedes descomentar esto. */}
     </div>
   );
 }
-
+{/* Debo modificar este archivo: Recuerdo del yo del futuro, recuerde modificar div classname container, discutirlo si dejarlo en navbar, homepage, etc. Es bueno a nivel semantico?*/}
 export { MainLayout };
